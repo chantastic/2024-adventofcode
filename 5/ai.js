1545f2810884537581a4348b2aaa4f2032fe1079
Michael Chan <mijoch@gmail.com>
2024-12-12 19:49:22 -0800

    add ai's solution for day 5

--- 5/ai.js
+++ 5/ai.js
@@ -13,9 +13,11 @@ function parseInput(input) {
 	return { rules, updates };
 }
 
+
 function isCorrectOrder(update, rules) {
 	for (let i = 0; i < update.length; i++) {
 		for (let j = i + 1; j < update.length; j++) {
+			console.log(i, j);
 			if (rules.has(update[j]) && rules.get(update[j]).has(update[i])) {
 				return false;
 			}
@@ -28,15 +30,34 @@ function findMiddlePage(update) {
 	return update[Math.floor(update.length / 2)];
 }
 
+function sortUpdate(update, rules) {
+	return update.sort((a, b) => {
+		if (rules.has(b) && rules.get(b).has(a)) return 1;
+		if (rules.has(a) && rules.get(a).has(b)) return -1;
+		return 0;
+	});
+}
+
 function processUpdates(rules, updates) {
 	let sum = 0;
 	for (let update of updates) {
-		if (isCorrectOrder(update, rules)) {
-			sum += findMiddlePage(update);
+		if (!isCorrectOrder(update, rules)) {
+			const sortedUpdate = sortUpdate([...update], rules);
+			sum += findMiddlePage(sortedUpdate);
 		}
 	}
 	return sum;
 }
+
+//function processUpdates(rules, updates) {
+//	let sum = 0;
+//	for (let update of updates) {
+//		if (isCorrectOrder(update, rules)) {
+//			sum += findMiddlePage(update);
+//		}
+//	}
+//	return sum;
+//}
 
 const { rules, updates } = parseInput(input);
 const result = processUpdates(rules, updates);
